6ddea4ced0a31a44f56d7196c693d60d475dd7cf
unknown <shannon.oram@gmail.com>
2013-12-01 12:33:10 +1100

    project setup changes

---
+++ Launch Delphi with Lucidity Project.bat
@@ -0,0 +1,7 @@
+:: Launch delphi with a custom registery root. 
+:: http://delphi.wikia.com/wiki/IDE_Command_Line_Switches
+start bds.exe "S:\Delphi\LucidityProjectGroup.groupproj" -r Lucidity 
+
+
+
+
