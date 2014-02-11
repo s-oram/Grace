e5c086548c200e0889effe7300e769af3f7c21b6
Shannon Matthews <shannon@onesmallclue.com>
2014-02-11 12:15:45 +1100

    add LucidityGui.Scope.SignalRecorder

---
+++ Delphi/LucidityGuiComponents/source/LucidityGui.Scope.SignalRecorder.pas
@@ -0,0 +1,15 @@
+unit LucidityGui.Scope.SignalRecorder;
+
+interface
+
+uses
+  VamLib.MoreTypes;
+
+type
+  IScopeSignalRecorder = interface
+    ['{A01F1BD5-F4DD-48A2-A466-5AD7C9195647}']
+  end;
+
+implementation
+
+end.
