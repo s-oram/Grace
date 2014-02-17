5a7ae790dbf4e8ca8034662c5b40a4f1891072ae
Shannon Matthews <shannon@onesmallclue.com>
2014-02-17 19:52:13 +1100

    add IVectorSequencerDataObject

--- Delphi/LucidityGuiComponents/source/LucidityGui.VectorSequence.pas
+++ Delphi/LucidityGuiComponents/source/LucidityGui.VectorSequence.pas
@@ -12,6 +12,12 @@ const
   kMaxSeqLength = 64;
 
 type
+  IVectorSequenceDataObject = interface
+    ['{2CB04233-08C0-425A-B122-C8D476A5D50F}']
+    function GetStepValue(Index : integer):single;
+    procedure SetStepValue(Index : integer; Value:single);
+  end;
+
   TLucidityVectorSequence = class(TVamWinControl)
   private
     fOnChanged: TNotifyEvent;
