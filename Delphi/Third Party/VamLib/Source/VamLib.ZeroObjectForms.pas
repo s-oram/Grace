6508df060d17d9df5bac7315d71798d5a229527c
Shannon Matthews <shannon@onesmallclue.com>
2014-02-20 14:41:47 +1100

    deleted ZeroObjectForm contents.
    this is not being used and doesn't work as expected.

--- Delphi/Third Party/VamLib/Source/VamLib.ZeroObjectForms.pas
+++ Delphi/Third Party/VamLib/Source/VamLib.ZeroObjectForms.pas
@@ -6,47 +6,9 @@ uses
   Vcl.Forms,
   VamLib.ZeroObject;
 
-type
-  TZeroObjectFrame = class(TFrame, IZeroObject)
-  protected
-    FMotherShip : IMotherShip;
-    procedure ClearMotherShipReference;
-    procedure ProcessZeroObjectMessage(MsgID:cardinal; Data:Pointer); virtual;
-  public
-    destructor Destroy; override;
-
-    procedure RegisterWithMotherShip(const Mothership:IMotherShip);
-  end;
+//TODO: Delete this unit.
 
 implementation
-
-{ TZeroObjectFrame }
-
-destructor TZeroObjectFrame.Destroy;
-begin
-  if (assigned(FMotherShip)) then
-  begin
-    FMotherShip.DeregisterZeroObject(self);
-  end;
-
-  inherited;
-end;
-
-procedure TZeroObjectFrame.ClearMotherShipReference;
-begin
-  FMotherShip := nil;
-end;
-
-procedure TZeroObjectFrame.RegisterWithMotherShip(const Mothership: IMotherShip);
-begin
-  FMotherShip := MotherShip;
-  FMotherShip.RegisterZeroObject(self);
-end;
-
-procedure TZeroObjectFrame.ProcessZeroObjectMessage(MsgID: cardinal; Data: Pointer);
-begin
-
-end;
 
 
 end.
