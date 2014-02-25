226f3aa25cefd10950ee4ee58da7935a8a1e1a42
Shannon Matthews <shannon@onesmallclue.com>
2014-02-25 14:55:25 +1100

    chaged TModulatedPar structure.

--- Delphi/Lucidity/Lucidity.Types.pas
+++ Delphi/Lucidity/Lucidity.Types.pas
@@ -14,8 +14,23 @@ type
   // These structures are intended for storing the modulated parameter values.
   // I'm not entirely sure of the variable naming yet. It doesn't seem quite right.
   TModulatedPar = record
+    //Holds the actual parameter value. Range should be 0..1
     ParValue  : single;
+
+    // The above Parameter value with the modulation applied.
+    // range should be 0..1
+    ModulatedParValue : single;
+
+    //Stores the modulation amount for each mod slot.
     ModAmount : array[0..kModSlotCount-1] of single;
+
+    // ModMin / ModMax stores the manimum and maximum modulation depth when the
+    // considering all modulation sources. Given:
+    //  - Slot One = 30%
+    //  - Slot Two = 50%
+    //  - Slot Three = -10%
+    // Min Mod will be -10%
+    // Max Slot will be 50%
     ModMin    : single;
     ModMax    : single;
   end;
