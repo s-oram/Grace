c76b0292b4b20dda50a5c4ecf5f1e635f653bade
Shannon Matthews <shannon@onesmallclue.com>
2014-04-11 16:08:54 +1000

    bug fix: can now set the loop point modulation again.

--- Delphi/Lucidity/frames/uMiniSampleDisplayFrame.pas
+++ Delphi/Lucidity/frames/uMiniSampleDisplayFrame.pas
@@ -232,6 +232,7 @@ procedure TMiniSampleDisplayFrame.ProcessZeroObjectMessage(MsgID: cardinal; Data
 begin
   if MsgID = TLucidMsgID.Command_UpdateControlVisibility then UpdateControlVisibility;
   if MsgID = TLucidMsgID.Command_UpdateModMatrix         then UpdateModulation;
+  if MsgID = TLucidMsgID.ModSlotChanged                  then UpdateModulation;
   if MsgID = TLucidMsgID.SampleFocusChanged              then UpdateControlVisibility;
   if MsgID = TLucidMsgID.SampleOscTypeChanged            then UpdateControlVisibility;
   if MsgID = TLucidMsgID.LoopTypeChanged                 then UpdateSampleDisplayInfo;
