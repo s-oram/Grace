bf2d22ea9873d786923bbfde793f25bf48f57c95
Shannon Matthews <shannon@onesmallclue.com>
2014-04-23 10:45:28 +1000

    Bug Fix: copy more MtxVec dlls.

--- 05 Install MtxVec DLLs.bat
+++ 05 Install MtxVec DLLs.bat
@@ -8,9 +8,11 @@ xcopy /y "S:\MtxVec Dlls\Double64\libiomp5md.dll"  "C:\Windows\System32\"
 :: Copy 32bit dlls
 xcopy /y "S:\MtxVec Dlls\Single\bdss.dll"  "C:\Windows\SysWOW64\"
 xcopy /y "S:\MtxVec Dlls\Single\libiomp5md.dll"  "C:\Windows\SysWOW64\"
+xcopy /y "S:\MtxVec Dlls\Single\MtxVec.*.dll"  "C:\Windows\SysWOW64\"
 
 xcopy /y "S:\MtxVec Dlls\Double\bdss.dll"  "C:\Windows\SysWOW64\"
 xcopy /y "S:\MtxVec Dlls\Double\libiomp5md.dll"  "C:\Windows\SysWOW64\"
+xcopy /y "S:\MtxVec Dlls\Double\MtxVec.*.dll"  "C:\Windows\SysWOW64\"
 
 
 
