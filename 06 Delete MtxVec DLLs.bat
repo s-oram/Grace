02b3aa8785b4bacc74a296e1ba6b20a4d99ef8b4
Shannon Matthews <shannon@onesmallclue.com>
2014-04-23 10:10:37 +1000

    add MtxVec DLL install and delete batch file implementation.

---
+++ 06 Delete MtxVec DLLs.bat
@@ -0,0 +1,10 @@
+
+:: Delete 64bit dlls
+del "C:\Windows\System32\MtxVec.*.dll"
+del "C:\Windows\System32\bdss.dll"
+
+del "C:\Windows\System32\bdsp.dll"
+del "C:\Windows\System32\libiomp5md.dll"
+
+:: Delete 32bit dlls
+del "C:\Windows\SysWOW64\MtxVec.*.dll"
