5a0e1fd6ef85d816d471a4918202bb3553a88dbd
Shannon Matthews <shannon@onesmallclue.com>
2014-04-30 16:59:22 +1000

    deleted contents of LucidityUtils.pas
    This code is all out of date now that we are using the new
    Lucidity.PluginParameters.pas

--- Delphi/Lucidity/LucidityUtils.pas
+++ Delphi/Lucidity/LucidityUtils.pas
@@ -2,49 +2,6 @@ unit LucidityUtils;
 
 interface
 
-uses
-  uConstants;
-
-
- // TODO: Delete all of this as soon as posible.
-type
-  TParInfoEx = record
-    Name : string;
-    ModLinkIndex : integer;
-  end;
-
-var
-  ParInfoEx : array[0..kParameterCount-1] of TParInfoEx;
-
-function ModLinkIndexToParName(Index : integer):string;
-function ParNameToModLinkIndex(Name : string):integer;
-
 implementation
-
-function ModLinkIndexToParName(Index : integer):string;
-var
-  c1: Integer;
-begin
-  for c1 := 0 to kParameterCount-1 do
-  begin
-    if Index = ParInfoEx[c1].ModLinkIndex
-      then exit(ParInfoEx[c1].Name);
-  end;
-
-  result := '';
-end;
-
-function ParNameToModLinkIndex(Name : string):integer;
-var
-  c1: Integer;
-begin
-  for c1 := 0 to kParameterCount-1 do
-  begin
-    if Name = ParInfoEx[c1].Name
-      then exit(ParInfoEx[c1].ModLinkIndex);
-  end;
-
-  result := -1;
-end;
 
 end.
