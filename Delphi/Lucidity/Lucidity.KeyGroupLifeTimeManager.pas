3931cca4f647504ac70ff0c2b7aae39cf46e3bd1
Shannon Matthews <shannon@onesmallclue.com>
2014-05-03 23:35:17 +1000

    add KeyGroupLifeTimeManager unit.

--- Delphi/Lucidity/Lucidity.KeyGroupLifeTimeManager.pas
+++ Delphi/Lucidity/Lucidity.KeyGroupLifeTimeManager.pas
@@ -4,6 +4,8 @@ interface
 
 uses
   Classes,
+  VamLib.Types,
+  Lucidity.Types,
   Lucidity.Interfaces,
   VamLib.ZeroObject;
 
@@ -11,11 +13,15 @@ type
   TKeyGroupLifeTimeManager = class(TZeroObject)
   private
     KeyGroupList : TInterfaceList;
+    ListLock : TFixedCriticalSection;
   public
     constructor Create;
     destructor Destroy; override;
 
+    procedure Add(var KeyGroup : IKeyGroup);
+    function Request(const KeyGroupID : TKeyGroupID):IKeyGroup;
 
+    procedure Dispose(const KeyGroupID : TKeyGroupID);
   end;
 
 implementation
@@ -24,13 +30,63 @@ implementation
 
 constructor TKeyGroupLifeTimeManager.Create;
 begin
-
+  KeyGroupList := TInterfaceList.Create;
+  ListLock := TFixedCriticalSection.Create;
 end;
 
 destructor TKeyGroupLifeTimeManager.Destroy;
 begin
-
+  KeyGroupList.Free;
+  ListLock.Free;
   inherited;
 end;
+
+procedure TKeyGroupLifeTimeManager.Add(var KeyGroup: IKeyGroup);
+begin
+  ListLock.Acquire;
+  try
+    KeyGroupList.Add(KeyGroup);
+  finally
+    ListLock.Release;
+  end;
+end;
+
+function TKeyGroupLifeTimeManager.Request(const KeyGroupID: TKeyGroupID): IKeyGroup;
+var
+  c1: Integer;
+begin
+  ListLock.Acquire;
+  try
+    for c1 := 0 to KeyGroupList.Count-1 do
+    begin
+      if (KeyGroupList[c1] as IKeyGroup).GetID = KeyGroupID
+        then exit(KeyGroupList[c1] as IKeyGroup);
+    end;
+  finally
+    ListLock.Release;
+  end;
+  //== no match found ==
+  result := nil;
+end;
+
+procedure TKeyGroupLifeTimeManager.Dispose(const KeyGroupID: TKeyGroupID);
+var
+  c1: Integer;
+begin
+  ListLock.Acquire;
+  try
+    for c1 := 0 to KeyGroupList.Count-1 do
+    begin
+      if (KeyGroupList[c1] as IKeyGroup).GetID = KeyGroupID then
+      begin
+        KeyGroupList[c1] := nil;
+      end;
+    end;
+  finally
+    ListLock.Release;
+  end;
+end;
+
+
 
 end.
