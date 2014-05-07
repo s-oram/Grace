41c205a01bd45c6b75427f9a6606fcab4ed958be
Shannon Matthews <shannon@onesmallclue.com>
2014-05-07 13:34:37 +1000

    created a SendMessages() method to send messages to the GUI after...
    parameters have changed.

--- Delphi/Lucidity/Lucidity.PluginParameterController.pas
+++ Delphi/Lucidity/Lucidity.PluginParameterController.pas
@@ -13,8 +13,7 @@ uses
 type
   TPluginParameterController = class
   private
-    class procedure ApplyPluginParToKeyGroup(const aPlugin : TObject; const KeyGroup : IKeyGroup; const Par : TPluginParameter; const ParValue : single); static; inline;
-
+    class procedure SendMessages(const aPlugin : TObject; const Par : TPluginParameter; const ParValue : single); static;
   public
     class procedure SetPluginParameter(const aPlugin : TObject; const Scope : TParChangeScope; const KeyGroupName : string; const ParName : string; const ParValue : single); static; inline;
     class function GetPluginParameter(const aPlugin : TObject; const ParName : string):single; static; inline;
@@ -27,6 +26,8 @@ type
     class function GetParameterInfo(const aPlugin : TObject; const ParName : string) : TPluginParameterInfo; static; inline;
 
     class function GetPluginParameterVstInfo(const aPlugin : TObject; const ParName : string):TVstParameterInfo; static; inline;
+
+    class procedure ApplyPluginParToKeyGroup(const KeyGroup : IKeyGroup; const Par : TPluginParameter; const ParValue : single); static;
   end;
 
   // TODO: This class will contain all the code required to set/get the parameter values
@@ -86,136 +87,34 @@ begin
     VoicePar := (KeyGroup.GetObject as TKeyGroup).VoiceParameters;
 
     case Par of
-      TPluginParameter.PitchTracking:
-      begin
-        result := TPitchTrackingHelper.ToSingle(VoicePar.PitchTracking);
-      end;
-
-      TPluginParameter.SamplePlaybackType:
-      begin
-        result := TSamplePlaybackTypeHelper.ToSingle(VoicePar.SamplePlaybackType);
-      end;
-
-      TPluginParameter.SampleResetClockSource:
-      begin
-        result := TClockSourceHelper.ToSingle(VoicePar.SampleReset);
-      end;
-
-      TPluginParameter.SamplerLoopBounds:
-      begin
-        result := TSamplerLoopBoundsHelper.ToSingle(VoicePar.SamplerLoopBounds);
-      end;
-
-      TPluginParameter.SamplerLoopMode:
-      begin
-        result := TSamplerLoopModeHelper.ToSingle(VoicePar.SamplerLoopMode);
-      end;
-
-      TPluginParameter.AmpVelocity:
-      begin
-        result := TEnvVelocityDepthHelper.ToSingle(VoicePar.AmpVelocityDepth);
-      end;
-
-      TPluginParameter.FilterVelocity:
-      begin
-        result := TEnvVelocityDepthHelper.ToSingle(VoicePar.FilterVelocityDepth);
-      end;
-
-      TPluginParameter.FilterRouting:
-      begin
-        result := TFilterRoutingHelper.ToSingle(VoicePar.FilterRouting);
-      end;
-
-      TPluginParameter.Filter1Type:
-      begin
-        result := TFilterTypeHelper.ToSingle(VoicePar.Filter1Type);
-      end;
-
-      TPluginParameter.Filter2Type:
-      begin
-        result := TFilterTypeHelper.ToSingle(VoicePar.Filter2Type);
-      end;
-
-      TPluginParameter.Filter1KeyFollow:
-      begin
-        result := VoicePar.Filter1KeyFollow;
-      end;
-
-      TPluginParameter.Filter2KeyFollow:
-      begin
-        result := VoicePar.Filter2KeyFollow;
-      end;
-
-      TPluginParameter.Lfo1Shape:
-      begin
-        result := TLfoShapeHelper.ToSingle(VoicePar.LfoShape1);
-      end;
-
-      TPluginParameter.Lfo2Shape:
-      begin
-        result := TLfoShapeHelper.ToSingle(VoicePar.LfoShape2);
-      end;
-
-      TPluginParameter.Lfo1FreqMode:
-      begin
-        result := TLfoFreqModeHelper.ToSingle(VoicePar.LfoFreqMode1);
-      end;
-
-      TPluginParameter.Lfo2FreqMode:
-      begin
-        result := TLfoFreqModeHelper.ToSingle(VoicePar.LfoFreqMode2);
-      end;
-
-      TPluginParameter.Lfo1Range:
-      begin
-        result := TLfoRangeHelper.ToSingle(VoicePar.LfoRange1);
-      end;
-
-      TPluginParameter.Lfo2Range:
-      begin
-        result := TLfoRangeHelper.ToSingle(VoicePar.LfoRange2);
-      end;
-
-      TPluginParameter.Seq1Clock:
-      begin
-        result := TSequencerClockHelper.ToSingle(VoicePar.Seq1Clock);
-      end;
-
-      TPluginParameter.Seq1Direction:
-      begin
-        result := TStepSequencerDirectionHelper.ToSingle(VoicePar.Seq1Direction);
-      end;
-
-      TPluginParameter.Seq1Length:
-      begin
-        result := TStepSequencerLengthHelper.ToSingle(VoicePar.StepSeq1Length);
-      end;
-
-      TPluginParameter.Seq2Clock:
-      begin
-        result := TSequencerClockHelper.ToSingle(VoicePar.Seq2Clock);
-      end;
-
-      TPluginParameter.Seq2Direction:
-      begin
-        result := TStepSequencerDirectionHelper.ToSingle(VoicePar.Seq2Direction);
-      end;
-
-      TPluginParameter.Seq2Length:
-      begin
-        result := TStepSequencerLengthHelper.ToSingle(VoicePar.StepSeq2Length);
-      end;
-
-
-      //TPluginParameter.PreviewVolume: ;
-      //TPluginParameter.Preview: ;
+      TPluginParameter.PitchTracking:          result := TPitchTrackingHelper.ToSingle(VoicePar.PitchTracking);
+      TPluginParameter.SamplePlaybackType:     result := TSamplePlaybackTypeHelper.ToSingle(VoicePar.SamplePlaybackType);
+      TPluginParameter.SampleResetClockSource: result := TClockSourceHelper.ToSingle(VoicePar.SampleReset);
+      TPluginParameter.SamplerLoopBounds:      result := TSamplerLoopBoundsHelper.ToSingle(VoicePar.SamplerLoopBounds);
+      TPluginParameter.SamplerLoopMode:        result := TSamplerLoopModeHelper.ToSingle(VoicePar.SamplerLoopMode);
+      TPluginParameter.AmpVelocity:            result := TEnvVelocityDepthHelper.ToSingle(VoicePar.AmpVelocityDepth);
+      TPluginParameter.FilterVelocity:         result := TEnvVelocityDepthHelper.ToSingle(VoicePar.FilterVelocityDepth);
+      TPluginParameter.FilterRouting:          result := TFilterRoutingHelper.ToSingle(VoicePar.FilterRouting);
+      TPluginParameter.Filter1Type:            result := TFilterTypeHelper.ToSingle(VoicePar.Filter1Type);
+      TPluginParameter.Filter2Type:            result := TFilterTypeHelper.ToSingle(VoicePar.Filter2Type);
+      TPluginParameter.Filter1KeyFollow:       result := VoicePar.Filter1KeyFollow;
+      TPluginParameter.Filter2KeyFollow:       result := VoicePar.Filter2KeyFollow;
+      TPluginParameter.Lfo1Shape:              result := TLfoShapeHelper.ToSingle(VoicePar.LfoShape1);
+      TPluginParameter.Lfo2Shape:              result := TLfoShapeHelper.ToSingle(VoicePar.LfoShape2);
+      TPluginParameter.Lfo1FreqMode:           result := TLfoFreqModeHelper.ToSingle(VoicePar.LfoFreqMode1);
+      TPluginParameter.Lfo2FreqMode:           result := TLfoFreqModeHelper.ToSingle(VoicePar.LfoFreqMode2);
+      TPluginParameter.Lfo1Range:              result := TLfoRangeHelper.ToSingle(VoicePar.LfoRange1);
+      TPluginParameter.Lfo2Range:              result := TLfoRangeHelper.ToSingle(VoicePar.LfoRange2);
+      TPluginParameter.Seq1Clock:              result := TSequencerClockHelper.ToSingle(VoicePar.Seq1Clock);
+      TPluginParameter.Seq1Direction:          result := TStepSequencerDirectionHelper.ToSingle(VoicePar.Seq1Direction);
+      TPluginParameter.Seq1Length:             result := TStepSequencerLengthHelper.ToSingle(VoicePar.StepSeq1Length);
+      TPluginParameter.Seq2Clock:              result := TSequencerClockHelper.ToSingle(VoicePar.Seq2Clock);
+      TPluginParameter.Seq2Direction:          result := TStepSequencerDirectionHelper.ToSingle(VoicePar.Seq2Direction);
+      TPluginParameter.Seq2Length:             result := TStepSequencerLengthHelper.ToSingle(VoicePar.StepSeq2Length);
     else
       raise Exception.Create('Type not handled.');
     end;
-
   end;
-
-
 end;
 
 class procedure TPluginParameterController.SetPluginParameter(
@@ -251,11 +150,12 @@ begin
       else
         raise Exception.Create('Type not handed.');
     end;
+
+    SendMessages(Plugin, Par, ParValue);
     exit; //=============================================>> exit >>=================>>
   end;
 
   //======= Voice / Key Group scope Parameters ======================
-
   case Scope of
     psGlobal:
     begin
@@ -264,23 +164,32 @@ begin
       for c1 := 0 to kgInfo.GetKeyGroupCount-1 do
       begin
         kg := kgInfo.GetKeyGroup(c1);
-        if assigned(kg)
-          then ApplyPluginParToKeyGroup(Plugin, kg, Par, ParValue);
+        if assigned(kg) then
+        begin
+          ApplyPluginParToKeyGroup(kg, Par, ParValue);
+          SendMessages(Plugin, Par, ParValue);
+        end;
       end;
     end;
 
     psFocusedKeyGroup:
     begin
       kg :=  Plugin.ActiveKeyGroup;
-      if assigned(kg)
-        then ApplyPluginParToKeyGroup(Plugin, kg, Par, ParValue);
+      if assigned(kg) then
+      begin
+        ApplyPluginParToKeyGroup(kg, Par, ParValue);
+        SendMessages(Plugin, Par, ParValue);
+      end;
     end;
 
     psKeyGroup:
     begin
       kg := Plugin.KeyGroups.FindSampleGroup(KeyGroupName);
-      if assigned(kg)
-        then ApplyPluginParToKeyGroup(Plugin, kg, Par, ParValue);
+      if assigned(kg) then
+      begin
+        ApplyPluginParToKeyGroup(kg, Par, ParValue);
+        SendMessages(Plugin, Par, ParValue);
+      end;
     end;
   else
     raise Exception.Create('Type not handled.');
@@ -289,17 +198,14 @@ begin
 end;
 
 class procedure TPluginParameterController.ApplyPluginParToKeyGroup(
-  const aPlugin : TObject;
   const KeyGroup: IKeyGroup;
   const Par: TPluginParameter;
   const ParValue: single);
 var
-  Plugin : TeePlugin;
   VoicePar : TLucidityVoiceParameterWrapper;
   ModParIndex : integer;
 begin
-  assert(aPlugin is TeePlugin);
-  Plugin := aPlugin as TeePlugin;
+  assert(IsGlobalPluginPar(Par) = false);
 
   if IsModPar(Par) then
   begin
@@ -309,142 +215,31 @@ begin
   begin
     VoicePar := (KeyGroup.GetObject as TKeyGroup).VoiceParameters;
 
-
     case Par of
-      TPluginParameter.PitchTracking:
-      begin
-        VoicePar.PitchTracking := TPitchTrackingHelper.ToEnum(ParValue);
-      end;
-
-      TPluginParameter.SamplePlaybackType:
-      begin
-        VoicePar.SamplePlaybackType   := TSamplePlaybackTypeHelper.ToEnum(ParValue);
-        Plugin.Globals.MotherShip.SendMessageUsingGuiThread(TLucidMsgID.SampleOscTypeChanged);
-      end;
-
-      TPluginParameter.SampleResetClockSource:
-      begin
-        VoicePar.SampleReset := TClockSourceHelper.ToEnum(ParValue);
-      end;
-
-      TPluginParameter.SamplerLoopBounds:
-      begin
-        VoicePar.SamplerLoopBounds := TSamplerLoopBoundsHelper.ToEnum(ParValue);
-
-      end;
-
-      TPluginParameter.SamplerLoopMode:
-      begin
-        VoicePar.SamplerLoopMode := TSamplerLoopModeHelper.ToEnum(ParValue);
-      end;
-
-      TPluginParameter.AmpVelocity:
-      begin
-        VoicePar.AmpVelocityDepth := TEnvVelocityDepthHelper.ToEnum(ParValue);
-      end;
-
-      TPluginParameter.FilterVelocity:
-      begin
-        VoicePar.FilterVelocityDepth := TEnvVelocityDepthHelper.ToEnum(ParValue);
-      end;
-
-      TPluginParameter.FilterRouting:
-      begin
-        VoicePar.FilterRouting := TFilterRoutingHelper.ToEnum(ParValue);
-        Plugin.Globals.MotherShip.SendMessageUsingGuiThread(TLucidMsgID.FilterChanged);
-        Plugin.Globals.MotherShip.SendMessageUsingGuiThread(TLucidMsgID.Command_UpdateScope);
-      end;
-
-      TPluginParameter.Filter1Type:
-      begin
-        VoicePar.Filter1Type := TFilterTypeHelper.ToEnum(ParValue);
-        Plugin.Globals.MotherShip.SendMessageUsingGuiThread(TLucidMsgID.FilterChanged);
-      end;
-
-      TPluginParameter.Filter2Type:
-      begin
-        VoicePar.Filter2Type := TFilterTypeHelper.ToEnum(ParValue);
-        Plugin.Globals.MotherShip.SendMessageUsingGuiThread(TLucidMsgID.FilterChanged);
-      end;
-
-      TPluginParameter.Filter1KeyFollow:
-      begin
-        VoicePar.Filter1KeyFollow := ParValue;
-      end;
-
-      TPluginParameter.Filter2KeyFollow:
-      begin
-        VoicePar.Filter2KeyFollow := ParValue;
-      end;
-
-      TPluginParameter.Lfo1Shape:
-      begin
-        VoicePar.LfoShape1 := TLfoShapeHelper.ToEnum(ParValue);
-        Plugin.Globals.MotherShip.SendMessageUsingGuiThread(TLucidMsgID.LfoChanged);
-      end;
-
-      TPluginParameter.Lfo2Shape:
-      begin
-        VoicePar.LfoShape2 := TLfoShapeHelper.ToEnum(ParValue);
-        Plugin.Globals.MotherShip.SendMessageUsingGuiThread(TLucidMsgID.LfoChanged);
-      end;
-
-      TPluginParameter.Lfo1FreqMode:
-      begin
-        VoicePar.LfoFreqMode1 := TLfoFreqModeHelper.ToEnum(ParValue);
-        Plugin.Globals.MotherShip.SendMessageUsingGuiThread(TLucidMsgID.LfoChanged);
-      end;
-
-      TPluginParameter.Lfo2FreqMode:
-      begin
-        VoicePar.LfoFreqMode2 := TLfoFreqModeHelper.ToEnum(ParValue);
-        Plugin.Globals.MotherShip.SendMessageUsingGuiThread(TLucidMsgID.LfoChanged);
-      end;
-
-      TPluginParameter.Lfo1Range:
-      begin
-        VoicePar.LfoRange1 := TLfoRangeHelper.ToEnum(ParValue);
-        Plugin.Globals.MotherShip.SendMessageUsingGuiThread(TLucidMsgID.LfoChanged);
-      end;
-
-      TPluginParameter.Lfo2Range:
-      begin
-        VoicePar.LfoRange2 := TLfoRangeHelper.ToEnum(ParValue);
-        Plugin.Globals.MotherShip.SendMessageUsingGuiThread(TLucidMsgID.LfoChanged);
-      end;
-
-      TPluginParameter.Seq1Clock:
-      begin
-        VoicePar.Seq1Clock := TSequencerClockHelper.ToEnum(ParValue);
-      end;
-
-      TPluginParameter.Seq1Direction:
-      begin
-        VoicePar.Seq1Direction := TStepSequencerDirectionHelper.ToEnum(ParValue);
-      end;
-
-      TPluginParameter.Seq1Length:
-      begin
-        VoicePar.StepSeq1Length := TStepSequencerLengthHelper.ToEnum(ParValue);
-      end;
-
-      TPluginParameter.Seq2Clock:
-      begin
-        VoicePar.Seq2Clock := TSequencerClockHelper.ToEnum(ParValue);
-      end;
-
-      TPluginParameter.Seq2Direction:
-      begin
-        VoicePar.Seq2Direction := TStepSequencerDirectionHelper.ToEnum(ParValue);
-      end;
-
-      TPluginParameter.Seq2Length:
-      begin
-        VoicePar.StepSeq2Length := TStepSequencerLengthHelper.ToEnum(ParValue);
-      end;
-
-      //TPluginParameter.PreviewVolume: ;
-      //TPluginParameter.Preview: ;
+      TPluginParameter.PitchTracking:          VoicePar.PitchTracking       := TPitchTrackingHelper.ToEnum(ParValue);
+      TPluginParameter.SamplePlaybackType:     VoicePar.SamplePlaybackType  := TSamplePlaybackTypeHelper.ToEnum(ParValue);
+      TPluginParameter.SampleResetClockSource: VoicePar.SampleReset         := TClockSourceHelper.ToEnum(ParValue);
+      TPluginParameter.SamplerLoopBounds:      VoicePar.SamplerLoopBounds   := TSamplerLoopBoundsHelper.ToEnum(ParValue);
+      TPluginParameter.SamplerLoopMode:        VoicePar.SamplerLoopMode     := TSamplerLoopModeHelper.ToEnum(ParValue);
+      TPluginParameter.AmpVelocity:            VoicePar.AmpVelocityDepth    := TEnvVelocityDepthHelper.ToEnum(ParValue);
+      TPluginParameter.FilterVelocity:         VoicePar.FilterVelocityDepth := TEnvVelocityDepthHelper.ToEnum(ParValue);
+      TPluginParameter.FilterRouting:          VoicePar.FilterRouting       := TFilterRoutingHelper.ToEnum(ParValue);
+      TPluginParameter.Filter1Type:            VoicePar.Filter1Type         := TFilterTypeHelper.ToEnum(ParValue);
+      TPluginParameter.Filter2Type:            VoicePar.Filter2Type         := TFilterTypeHelper.ToEnum(ParValue);
+      TPluginParameter.Filter1KeyFollow:       VoicePar.Filter1KeyFollow    := ParValue;
+      TPluginParameter.Filter2KeyFollow:       VoicePar.Filter2KeyFollow    := ParValue;
+      TPluginParameter.Lfo1Shape:              VoicePar.LfoShape1           := TLfoShapeHelper.ToEnum(ParValue);
+      TPluginParameter.Lfo2Shape:              VoicePar.LfoShape2           := TLfoShapeHelper.ToEnum(ParValue);
+      TPluginParameter.Lfo1FreqMode:           VoicePar.LfoFreqMode1        := TLfoFreqModeHelper.ToEnum(ParValue);
+      TPluginParameter.Lfo2FreqMode:           VoicePar.LfoFreqMode2        := TLfoFreqModeHelper.ToEnum(ParValue);
+      TPluginParameter.Lfo1Range:              VoicePar.LfoRange1           := TLfoRangeHelper.ToEnum(ParValue);
+      TPluginParameter.Lfo2Range:              VoicePar.LfoRange2           := TLfoRangeHelper.ToEnum(ParValue);
+      TPluginParameter.Seq1Clock:              VoicePar.Seq1Clock           := TSequencerClockHelper.ToEnum(ParValue);
+      TPluginParameter.Seq1Direction:          VoicePar.Seq1Direction       := TStepSequencerDirectionHelper.ToEnum(ParValue);
+      TPluginParameter.Seq1Length:             VoicePar.StepSeq1Length      := TStepSequencerLengthHelper.ToEnum(ParValue);
+      TPluginParameter.Seq2Clock:              VoicePar.Seq2Clock           := TSequencerClockHelper.ToEnum(ParValue);
+      TPluginParameter.Seq2Direction:          VoicePar.Seq2Direction       := TStepSequencerDirectionHelper.ToEnum(ParValue);
+      TPluginParameter.Seq2Length:             VoicePar.StepSeq2Length      := TStepSequencerLengthHelper.ToEnum(ParValue);
     else
       raise Exception.Create('Type not handled.');
     end;
@@ -578,6 +373,41 @@ begin
   result.Display := '';
   result.Units   := '';
 end;
+
+class procedure TPluginParameterController.SendMessages(const aPlugin: TObject; const Par: TPluginParameter; const ParValue: single);
+var
+  Plugin : TeePlugin;
+begin
+  // NOTE: Some parameter changes require messages to be sent afterwards so
+  // the GUI (or other things can be updated. This set up is kind of the result
+  // of some decisions made in older code. It might be possible to have
+  // these changes sent from the source of the changes.
+
+  assert(aPlugin is TeePlugin);
+  Plugin := aPlugin as TeePlugin;
+
+  if IsModPar(Par) then exit;
+
+  case Par of
+    TPluginParameter.FilterRouting:
+    begin
+      Plugin.Globals.MotherShip.MsgVclTS(TLucidMsgID.FilterChanged);
+      Plugin.Globals.MotherShip.MsgVclTS(TLucidMsgID.Command_UpdateScope);
+    end;
+    TPluginParameter.SamplePlaybackType: Plugin.Globals.MotherShip.MsgVclTS(TLucidMsgID.SampleOscTypeChanged);
+    TPluginParameter.Filter1Type:        Plugin.Globals.MotherShip.MsgVclTS(TLucidMsgID.FilterChanged);
+    TPluginParameter.Filter2Type:        Plugin.Globals.MotherShip.MsgVclTS(TLucidMsgID.FilterChanged);
+    TPluginParameter.Lfo1Shape:          Plugin.Globals.MotherShip.MsgVclTS(TLucidMsgID.LfoChanged);
+    TPluginParameter.Lfo2Shape:          Plugin.Globals.MotherShip.MsgVclTS(TLucidMsgID.LfoChanged);
+    TPluginParameter.Lfo1FreqMode:       Plugin.Globals.MotherShip.MsgVclTS(TLucidMsgID.LfoChanged);
+    TPluginParameter.Lfo2FreqMode:       Plugin.Globals.MotherShip.MsgVclTS(TLucidMsgID.LfoChanged);
+    TPluginParameter.Lfo1Range:          Plugin.Globals.MotherShip.MsgVclTS(TLucidMsgID.LfoChanged);
+    TPluginParameter.Lfo2Range:          Plugin.Globals.MotherShip.MsgVclTS(TLucidMsgID.LfoChanged);
+  end;
+
+end;
+
+
 
 
 
