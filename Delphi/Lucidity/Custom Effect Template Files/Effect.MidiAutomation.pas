a550e637fd0bf847d7a184b735bcd8f495b893fa
Shannon Matthews <shannon@onesmallclue.com>
2014-05-09 00:21:39 +1000

    adding more methods...

--- Delphi/Lucidity/Custom Effect Template Files/Effect.MidiAutomation.pas
+++ Delphi/Lucidity/Custom Effect Template Files/Effect.MidiAutomation.pas
@@ -3,12 +3,10 @@ unit Effect.MidiAutomation;
 interface
 
 uses
-
   Lucidity.Interfaces,
   eeMidiAutomationV2;
 
 type
-
   IMidiBinding = interface(ICustomMidiBinding)
     ['{10775DF2-C58D-4EC6-A687-7DA1BB4C8CE9}']
     function GetParName : string;
@@ -30,6 +28,16 @@ type
   public
     constructor Create; override;
     destructor Destroy; override;
+
+
+    // TODO: write method to save/load automation to a XML node.
+
+    // TODO: write method to get MIDI learn CC for a parameter
+    function FindBinding(const ParName : string):IMidiBinding;
+    procedure ClearBinding(const ParName : string);
+
+
+    // TODO: write method to clear parameter for a MIDI cc.
   end;
 
 implementation
@@ -47,6 +55,35 @@ destructor TMidiAutomation.Destroy;
 begin
 
   inherited;
+end;
+
+function TMidiAutomation.FindBinding(const ParName: string): IMidiBinding;
+var
+  c1 : integer;
+begin
+  for c1 := BindingList.Count-1 downto 0 do
+  begin
+    if (BindingList[c1] as IMidiBinding).GetParName = ParName
+      then exit(BindingList[c1] as IMidiBinding);
+  end;
+
+  // no match found
+  result := nil;
+end;
+
+procedure TMidiAutomation.ClearBinding(const ParName: string);
+var
+  c1 : integer;
+begin
+  for c1 := BindingList.Count-1 downto 0 do
+  begin
+    if (BindingList[c1] as IMidiBinding).GetParName = ParName then
+    begin
+      BindingList.Delete(c1);
+      //TODO: possible need to make this thread-safe - maybe by executing in
+      // the audio thread actions...
+    end;
+  end;
 end;
 
 { TMidiBinding }
