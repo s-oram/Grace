74ca16ed1012485476d1df622c4896c4f413f73b
Shannon Matthews <shannon@onesmallclue.com>
2014-07-06 14:01:42 +1000

    notes added.

--- Delphi/Lucidity/Lucidity.Types.pas
+++ Delphi/Lucidity/Lucidity.Types.pas
@@ -126,6 +126,17 @@ type
                    smSampleStartModMarker, smSampleEndModMarker, smLoopStartModMarker, smLoopEndModMarker);
 
 
+
+  // TParChangeScope = Parameter Change Scope.
+  // Setting/Getting a plugin parameters value becomes complicated
+  // when a parameter can be dynmaically mapped to different 'focused'
+  // elements of a multi-timbral type patch. For Example: The ADSR
+  // envelope knobs might be applied to the focused sample, not all
+  // loaded samples.
+  // By controst parameter changes sometimes need to be global and
+  // applied to all 'layers' of a patch.
+  // Generally parameter changes from MIDI and Published VST parameters
+  // are "Global" while the GUI normally triggers "Focused" parameter changes.
   TParChangeScope = (psGlobal, psFocused);
 
 
