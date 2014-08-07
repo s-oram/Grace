82436ef60630a793ec307ec96fd99f8dfa617068
Shannon Matthews <shannon@onesmallclue.com>
2014-08-07 19:13:20 +1000

    add JCL files.

---
+++ Delphi/Third Party/jcl-2.3.1.4197/examples/windows/delphitools/peviewer/PeViewer.dpr
@@ -0,0 +1,36 @@
+program PeViewer;
+
+{$I jcl.inc}
+
+uses
+  Forms,
+  SysUtils,
+  D6MdiMsgFix in '..\Common\D6MdiMsgFix.pas',
+  PeViewerMain in 'PeViewerMain.pas' {MainForm},
+  PeDump in 'PeDump.pas' {PeDumpChild},
+  PeSearch in 'PeSearch.pas' {PeSearchChild},
+  PeViewer_TLB in 'PeViewer_TLB.pas',
+  PeViewerControl in 'PeViewerControl.pas' {PeViewerControl: CoClass},
+  PeResource in 'PeResource.pas',
+  PeResView in 'PeResView.pas' {PeResViewChild},
+  ToolsUtils in '..\Common\ToolsUtils.pas',
+  About in '..\Common\About.pas' {AboutBox},
+  PeGenDef in 'PeGenDef.pas' {PeGenDefChild},
+  FindDlg in '..\Common\FindDlg.pas' {FindTextForm},
+  ExceptDlg in '..\..\..\..\experts\repository\ExceptionDialog\StandardDialogs\ExceptDlg.pas' {ExceptionDialog}
+  {$IFNDEF RTL230_UP} 
+  , SHDocVw_TLB in '..\Common\SHDocVw_TLB.pas'
+  {$ENDIF ~RTL230_UP}
+  ;
+
+{$R *.TLB}
+
+{$R *.RES}
+{$R ..\..\..\..\source\windows\JclCommCtrlAsInvoker.res}
+
+begin
+  Application.Initialize;
+  Application.Title := 'PE Viewer';
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
