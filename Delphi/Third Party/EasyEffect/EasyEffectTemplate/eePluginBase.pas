94cba95014f7d0408b8bb7c25795b70f595ed76d
Shannon Matthews <shannon@onesmallclue.com>
2014-10-09 20:24:38 +1100

    notes deleted.

--- Delphi/Third Party/EasyEffect/EasyEffectTemplate/eePluginBase.pas
+++ Delphi/Third Party/EasyEffect/EasyEffectTemplate/eePluginBase.pas
@@ -149,11 +149,6 @@ begin
 
   fGlobals := aGlobals;
 
-  // NOTE: An object can't seem to register itself as a ZeroObject if it also creates the
-  // MotherShip class. I'm not sure why. But when using 12 or more instances in
-  // Renoise, Lucidity will raise AV errors when being free'ed.
-  //Globals.MotherShip.RegisterZeroObject(self, TZeroObjectRank.Main);
-
   AudioEffect := TVstAudioEffect.Create;
 
   fIsSuspended := true;
@@ -167,9 +162,6 @@ end;
 
 destructor TeePluginBase.Destroy;
 begin
-  // NOTE: see the create method for details
-  //Globals.MotherShip.DeregisterZeroObject(self);
-
   SetLength(Inputs,0);
   SetLength(Outputs,0);
 
