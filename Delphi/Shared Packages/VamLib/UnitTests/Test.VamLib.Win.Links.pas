59d61538fe61731f9b66b277d013c48be80b4bec
Shannon Oram <shannon@onesmallclue.com>
2015-09-28 13:25:46 +1000

    add test. currently doesn't pass.

--- Delphi/Shared Packages/VamLib/UnitTests/Test.VamLib.Win.Links.pas
+++ Delphi/Shared Packages/VamLib/UnitTests/Test.VamLib.Win.Links.pas
@@ -13,20 +13,17 @@ type
     procedure TearDown; override;
 
     [Test]
-    procedure GetLinkTarget;
+    procedure GetLinkTarget_Test;
   end;
 
 implementation
 
 uses
+  SysUtils,
+  WatchTower.Confirm,
   VamLib.Win.Links;
 
 { TVamLib_Win_Links_Test }
-
-procedure TVamLib_Win_Links_Test.GetLinkTarget;
-begin
-
-end;
 
 procedure TVamLib_Win_Links_Test.Setup;
 begin
@@ -39,5 +36,24 @@ begin
   inherited;
 
 end;
+
+procedure TVamLib_Win_Links_Test.GetLinkTarget_Test;
+var
+  LinkFile : string;
+  TargetFile : string;
+  x : string;
+begin
+  LinkFile   := 'S:\Delphi\Shared Packages\VamLib\UnitTests\Test.VamLib.Win.Links\Link To Target File.lnk';
+  TargetFile := 'S:\Delphi\Shared Packages\VamLib\UnitTests\Test.VamLib.Win.Links\Target File.txt';
+
+  Confirm.IsTrue(FileExists(LinkFile));
+  Confirm.IsTrue(FileExists(TargetFile));
+
+  x := GetLinkTarget(LinkFile);
+
+
+end;
+
+
 
 end.
