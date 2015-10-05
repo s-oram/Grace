dd82ed6af5ee21d7d3ec2e73c926f865d119eb96
Shannon Oram <shannon@onesmallclue.com>
2015-10-05 15:17:07 +1100

    setting plugin parameters seems to work when testing..

--- Delphi/Lucidity/UnitTests/Test.LucidityBug.SetGuiParameter.pas
+++ Delphi/Lucidity/UnitTests/Test.LucidityBug.SetGuiParameter.pas
@@ -28,7 +28,9 @@ implementation
 uses
   SysUtils,
   VamLib.MoreTypes,
-  WatchTower.Confirm;
+  WatchTower.Confirm,
+  Lucidity.PluginParameters,
+  eeTypes;
 
 { TLucidity_SetGUIPar }
 
@@ -36,22 +38,62 @@ procedure TLucidityBug_SetGUIPar.Setup;
 begin
   inherited;
   Globals := TGlobals.Create;
-
-
+  Plug := TeePlugin.Create(Globals);
 
 end;
 
 procedure TLucidityBug_SetGUIPar.TearDown;
 begin
   inherited;
+  Plug.Free;
   Globals.Free;
 end;
 
 procedure TLucidityBug_SetGUIPar.AttempToSetGUIParameter;
+var
+  ParID    : TPluginParameterID;
+  ParValue : single;
+  RetrievedValue : single;
 begin
   // BUG:
   // Grace is not setting parameters from the GUI correctly.
-  // Reproduce: Create a new
+  // Steps to reproduce:
+  //   a) Create a grace instance in DAW.
+  //   b) Create two groups. try to adjust attack parameter for both groups. Notice
+  //      that both groups always have the same attack parameter. The groups should
+  //      be able to have different attack parameters.
+
+  Plug.KeyGroups.NewKeyGroup('Group1');
+  Plug.KeyGroups.NewKeyGroup('Group2');
+
+  ParID := PluginParNameToID(PluginParToName(TPluginParameter.AmpAttack));
+
+  Plug.FocusKeyGroup('Group1');
+  ParValue := 0.3;
+  Plug.SetPluginParameter(ParID, ParValue, TParChangeScope.psFocused);
+
+
+  Plug.FocusKeyGroup('Group2');
+  ParValue := 0.7;
+  Plug.SetPluginParameter(ParID, ParValue, TParChangeScope.psFocused);
+
+
+
+  Plug.FocusKeyGroup('Group1');
+  ParValue := 0.3;
+  RetrievedValue := Plug.GetPluginParameter(ParID);
+  Confirm.IsTrue(RetrievedValue = ParValue);
+
+
+
+  Plug.FocusKeyGroup('Group2');
+  ParValue := 0.7;
+  RetrievedValue := Plug.GetPluginParameter(ParID);
+  Confirm.IsTrue(RetrievedValue = ParValue);
+
+
+
+  //Plug.SaveProgramToFileWithoutSamples('C:\Users\vam\Desktop\Default.lpg');
 end;
 
 
