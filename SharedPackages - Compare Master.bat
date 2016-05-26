33ca7bc3c32702070d3f766af983e73a1d865589
Shannon Oram <shannon.oram@gmail.com>
2016-05-26 11:46:54 +1000

    clean up.

--- SharedPackages - Compare Master.bat
+++ SharedPackages - Compare Master.bat
@@ -1,1 +1,3 @@
-start "" "C:\Program Files (x86)\Beyond Compare 3\BCompare.exe" "compare shared package dir"
+start "" "C:\Program Files (x86)\Beyond Compare 4\BCompare.exe" "Compare SharedPackages to Master"
+
+::start "" "C:\Program Files (x86)\Beyond Compare 4\BCompare.exe" "D:\Delphi Projects\Shared Packages" "S:\Delphi\Shared Packages" 
