c9b9d11537b5c3da7dea524576d139694e7c9634
Shannon Oram <shannon.oram@gmail.com>
2016-05-29 16:53:26 +1000

    update SharedPackage comparison.

--- SharedPackages - Compare Master.bat
+++ SharedPackages - Compare Master.bat
@@ -1,3 +1,1 @@
 start "" "C:\Program Files (x86)\Beyond Compare 4\BCompare.exe" "Compare SharedPackages to Master"
-
-::start "" "C:\Program Files (x86)\Beyond Compare 4\BCompare.exe" "D:\Delphi Projects\Shared Packages" "S:\Delphi\Shared Packages" 
